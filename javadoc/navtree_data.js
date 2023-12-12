ce9689704963faa862c143a6d6cf1f67937f2e7e
Greg Moens <greg_moens@vuzix.com>
2023-12-12 15:58:05 -0500

    1.96 javadoc updates

--- javadoc/navtree_data.js
+++ javadoc/navtree_data.js
@@ -1,5 +1,6 @@
 var NAVTREE_DATA =
-[ [ "com.vuzix.sdk.speechrecognitionservice", "reference/com/vuzix/sdk/speechrecognitionservice/package-summary.html", [ [ "Classes", null, [ [ "VuzixSpeechClient", "reference/com/vuzix/sdk/speechrecognitionservice/VuzixSpeechClient.html", null, null, null ] ]
+[ [ "com.vuzix.sdk.speechrecognitionservice", "reference/com/vuzix/sdk/speechrecognitionservice/package-summary.html", [ [ "Classes", null, [ [ "VuzixBubbleDelegate", "reference/com/vuzix/sdk/speechrecognitionservice/VuzixBubbleDelegate.html", null, null, null ], [ "VuzixSpeechClient", "reference/com/vuzix/sdk/speechrecognitionservice/VuzixSpeechClient.html", null, null, null ] ]
+, null, null ], [ "Enums", null, [ [ "VuzixBubbleDelegate.Alignment", "reference/com/vuzix/sdk/speechrecognitionservice/VuzixBubbleDelegate.Alignment.html", null, null, null ] ]
 , null, null ] ]
 , null, null ] ]
 
