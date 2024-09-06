68af03d2a65956a6278c5aca6f8d9daf0abd883d
bkeller-vuzix <98604695+bkeller-vuzix@users.noreply.github.com>
2024-09-06 15:51:58 -0400

    Javadocs for v1.97

--- javadoc/navtree_data.js
+++ javadoc/navtree_data.js
@@ -1,6 +1,6 @@
 var NAVTREE_DATA =
 [ [ "com.vuzix.sdk.speechrecognitionservice", "reference/com/vuzix/sdk/speechrecognitionservice/package-summary.html", [ [ "Classes", null, [ [ "VuzixBubbleDelegate", "reference/com/vuzix/sdk/speechrecognitionservice/VuzixBubbleDelegate.html", null, null, null ], [ "VuzixSpeechClient", "reference/com/vuzix/sdk/speechrecognitionservice/VuzixSpeechClient.html", null, null, null ] ]
-, null, null ], [ "Enums", null, [ [ "VuzixBubbleDelegate.Alignment", "reference/com/vuzix/sdk/speechrecognitionservice/VuzixBubbleDelegate.Alignment.html", null, null, null ] ]
+, null, null ], [ "Enums", null, [ [ "VuzixBubbleDelegate.Alignment", "reference/com/vuzix/sdk/speechrecognitionservice/VuzixBubbleDelegate.Alignment.html", null, null, null ], [ "VuzixSpeechClient.SpeechTolerance", "reference/com/vuzix/sdk/speechrecognitionservice/VuzixSpeechClient.SpeechTolerance.html", null, null, null ] ]
 , null, null ] ]
 , null, null ] ]
 
--- javadoc/reference/lists.js
+++ javadoc/reference/lists.js
@@ -2,6 +2,7 @@ var DATA = [
       { id:0, label:"com.vuzix.sdk.speechrecognitionservice", link:"reference/com/vuzix/sdk/speechrecognitionservice/package-summary.html", type:"package", deprecated:"false" },
       { id:1, label:"com.vuzix.sdk.speechrecognitionservice.VuzixBubbleDelegate", link:"reference/com/vuzix/sdk/speechrecognitionservice/VuzixBubbleDelegate.html", type:"class", deprecated:"false" },
       { id:2, label:"com.vuzix.sdk.speechrecognitionservice.VuzixBubbleDelegate.Alignment", link:"reference/com/vuzix/sdk/speechrecognitionservice/VuzixBubbleDelegate.Alignment.html", type:"class", deprecated:"false" },
-      { id:3, label:"com.vuzix.sdk.speechrecognitionservice.VuzixSpeechClient", link:"reference/com/vuzix/sdk/speechrecognitionservice/VuzixSpeechClient.html", type:"class", deprecated:"false" }
+      { id:3, label:"com.vuzix.sdk.speechrecognitionservice.VuzixSpeechClient", link:"reference/com/vuzix/sdk/speechrecognitionservice/VuzixSpeechClient.html", type:"class", deprecated:"false" },
+      { id:4, label:"com.vuzix.sdk.speechrecognitionservice.VuzixSpeechClient.SpeechTolerance", link:"reference/com/vuzix/sdk/speechrecognitionservice/VuzixSpeechClient.SpeechTolerance.html", type:"class", deprecated:"false" }
 
     ];
